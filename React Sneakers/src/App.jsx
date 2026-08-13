d9718cca91243c0e9a287fe3accd27cc316a50ae
Reilms <nazarmederbekov1@gmail.com>
2026-08-14 01:53:12 +0600

    lesson 5 #1

--- React Sneakers/src/App.jsx
+++ React Sneakers/src/App.jsx
@@ -1,44 +1,73 @@
 import './App.css'
+import axios from 'axios'
 import Header from './components/Header';
 import CartSidebar from './components/CartSidebar';
 import ProductCard from './components/ProductCard'
 import { useState, useEffect } from "react";
 import { FiSearch } from "react-icons/fi";
+import { ImCross } from "react-icons/im";
 
 
 function App() {
 
     const [items, setItems] = useState([])
     const [cartItems, setCartItems] = useState([])
+    const [searchValue, setSearchValue] = useState('')
     const [openSidebar, setOpenSidebar] = useState(false)
 
     useEffect(() => {
-        fetch("https://6a77425c63e9caf860c3717b.mockapi.io/items")
+        //При запросе информации с бекенда можно использовать 1 из 2 способов: fetch() или axios. Обычно axios лучше
+        /* fetch("https://6a77425c63e9caf860c3717b.mockapi.io/items")
             .then(res => { return res.json() })
-            .then((json) => { setItems(json) })
-    })
+            .then((json) => { setItems(json) }) */
+
+        axios.get("https://6a77425c63e9caf860c3717b.mockapi.io/items")
+        .then((res) => {setItems(res.data)})
+        //Для корзины
+        axios.get("https://6a77425c63e9caf860c3717b.mockapi.io/cartItems")
+        .then((res) => {setCartItems(res.data)})
+    }, [])
 
     const addToCart = (obj) => {
+        axios.post("https://6a77425c63e9caf860c3717b.mockapi.io/cartItems", obj)
         setCartItems(prev => [...prev, obj])
     }
+
+    const removeFromCart = (id) => {
+        axios.delete(`https://6a77425c63e9caf860c3717b.mockapi.io/cartItems/${id}`)
+        setCartItems((prev) => prev.filter((item) => item.id !== id))
+    }
+
+    const onChangeOfSearch = (event) => {
+        setSearchValue(event.target.value)
+    }
+
 
     return (
         <>
             <div className='bg-white m-25 rounded-2xl'>
-                {openSidebar && <CartSidebar items={cartItems} onClose={() => setOpenSidebar(false)} />}
+                {openSidebar && <CartSidebar items={cartItems} onClose={() => setOpenSidebar(false)} onRemove={removeFromCart}/>}
                 <Header onClickCart={() => setOpenSidebar(true)} />
                 <div className='p-20 flex flex-col gap-10'>
 
                     <div className='flex items-center justify-between'>
-                        <h1 className='font-bold text-4xl'>Все кроссовки:</h1>
-                        <div className='flex items-center justify-center border border-gray-300 rounded-xl w-75 h-14 gap-3'>
-                            <FiSearch size={25} color='lightgray' /><input type="text" placeholder='Поиск...' className='outline-0' />
+                        <h1 className='font-bold text-4xl '>{searchValue ? `Поиск по запросу "${searchValue}"` : `Все кроссовки: `}</h1>
+                        <div className='flex items-center justify-center border border-gray-300 rounded-xl w-75 h-14 gap-3 relative'>
+                            <FiSearch size={25} color='lightgray' />
+                            <input type="text" placeholder='Поиск...' className='outline-0' value={searchValue} onChange={onChangeOfSearch}/>
+                            {searchValue && <span className='absolute right-0 mr-3 cursor-pointer overflow-auto bg-white flex items-center justify-center w-10 h-10'
+                            onClick={() => setSearchValue('')}>
+                                <ImCross color='lightgray' size={16}/>
+                            </span>}
                         </div>
                     </div>
 
                     <div className='flex flex-wrap items-center gap-18 '>
-                        {items.map(item => (
+                        {items
+                        .filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+                        .map((item, index) => (
                             <ProductCard
+                                key={index}
                                 title={item.title}
                                 price={item.price}
                                 img={item.img}
--- React Sneakers/src/components/CartSidebar.jsx
+++ React Sneakers/src/components/CartSidebar.jsx
@@ -1,7 +1,8 @@
 import { ImCross } from "react-icons/im";
-import { FaArrowRight } from "react-icons/fa6";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
+import { FaBoxOpen } from "react-icons/fa";
 
-function CartSidebar({onClose, items = [] }) {
+function CartSidebar({ onClose, onRemove, items = [] }) {
     return (
         <>
             <aside className='bg-white h-screen w-100 fixed right-0 top-0 border shadow-2xl z-10 p-7 flex flex-col'>
@@ -12,36 +13,53 @@ function CartSidebar({onClose, items = [] }) {
                     ><ImCross color='black' size={20} /></button>
                 </div>
 
-                <div className='flex flex-col flex-1 gap-5 overflow-auto scrollbar-none '>
-                    {items.map((obj) => (
-                        <div className='border-2 border-gray-200 rounded-xl flex items-center justify-betwee w-full h-30 gap-4 p-4'>
-                            <img src={obj.img} alt="" width={70} height={70} />
-                            <div className='flex flex-col gap-2'>
-                                <h3>{obj.title}</h3>
-                                <p className='font-bold'>{obj.price} сом</p>
-                            </div>
-                            <button className='border border-[#D3D3D3] min-w-9 min-h-9 flex items-center justify-center rounded-lg
-                        cursor-pointer'><ImCross color='lightgray' size={12} /></button>
+                {
+                    items.length > 0 ? (<>
+                        <div className='flex flex-col flex-1 gap-5 overflow-auto scrollbar-none '>
+                            {items.map((obj) => (
+                                <div className='border-2 border-gray-200 rounded-xl flex items-center justify-betwee w-full h-30 gap-4 p-4'>
+                                    <img src={obj.img} alt="" width={70} height={70} />
+                                    <div className='flex flex-col gap-2'>
+                                        <h3>{obj.title}</h3>
+                                        <p className='font-bold'>{obj.price} сом</p>
+                                    </div>
+                                    <button className='border border-[#D3D3D3] min-w-9 min-h-9 flex items-center justify-center rounded-lg cursor-pointer'
+                                        onClick={() => onRemove(obj.id)}>
+                                        <ImCross color='lightgray' size={12} />
+                                    </button>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
 
-                <div className='flex flex-col gap-5 mt-10'>
-                    <ul className='flex flex-col gap-4'>
-                        <li className='flex items-center justify-between gap-2'>
-                            <span>Итого:</span>
-                            <div className='h-px grow border border-dashed border-gray-400 relative top-2 '></div>
-                            <p >21.498 сом</p>
-                        </li>
-                        <li className='flex items-center justify-between gap-2'>
-                            <span>Налог 5%:</span>
-                            <div className='h-px grow border border-dashed border-gray-400 relative top-2 '></div>
-                            <p>1.074 сом</p>
-                        </li>
-                    </ul>
-                    <button className='h-15 w-full bg-green-600 rounded-2xl cursor-pointer flex items-center justify-center
-          hover:bg-green-700 duration-200'>Оформить заказ <FaArrowRight size={20} className='relative -right-15' /></button>
-                </div>
+                        <div className='flex flex-col gap-5 mt-10'>
+                            <ul className='flex flex-col gap-4'>
+                                <li className='flex items-center justify-between gap-2'>
+                                    <span>Итого:</span>
+                                    <div className='h-px grow border border-dashed border-gray-400 relative top-2 '></div>
+                                    <p >21.498 сом</p>
+                                </li>
+                                <li className='flex items-center justify-between gap-2'>
+                                    <span>Налог 5%:</span>
+                                    <div className='h-px grow border border-dashed border-gray-400 relative top-2 '></div>
+                                    <p>1.074 сом</p>
+                                </li>
+                            </ul>
+                            <button className='h-15 w-full bg-green-600 rounded-2xl cursor-pointer flex items-center justify-center 
+                            hover:bg-green-700 duration-200'>Оформить заказ <FaArrowRight size={20} className='relative -right-15' /></button>
+                        </div>
+                    </>)
+                        :
+                        (<div className="flex flex-col items-center justify-center h-full gap-8">
+                            <FaBoxOpen size={150} color="orange" />
+                            <div className="flex flex-col items-center justify-center gap-3">
+                                <h1 className="text-2xl font-semibold">Корзина пуста...</h1>
+                                <p className="text-center opacity-40 w-4/5">Добавьте хотя бы один товар чтобы оформить заказ</p>
+                            </div>
+                            <button className='h-15 w-full bg-green-600 rounded-2xl cursor-pointer flex items-center justify-center
+                            hover:bg-green-700 duration-200'><FaArrowLeft size={20} className='relative -left-15' /> Вернуться назад</button>
+                        </div>)
+                }
+
             </aside>
             <div className='bg-black h-screen w-screen fixed left-0 top-0 z-5 opacity-50'
                 onClick={onClose}
@@ -50,13 +68,3 @@ function CartSidebar({onClose, items = [] }) {
     )
 }
 export default CartSidebar
-
-{/* <div className='border-2 border-gray-200 rounded-xl flex items-center justify-betwee w-full h-30 gap-4 p-4'>
-                        <img src="/img/sneakers2.jpg" alt="" width={70} height={70} />
-                        <div className='flex flex-col gap-2'>
-                            <h3>Мужские Кроссовки Nike Air Max 270</h3>
-                            <p className='font-bold'>12.999 сом</p>
-                        </div>
-                        <button className='border border-[#D3D3D3] min-w-9 min-h-9 flex items-center justify-center rounded-lg
-                        cursor-pointer'><ImCross color='lightgray' size={12} /></button>
-                    </div> */}
